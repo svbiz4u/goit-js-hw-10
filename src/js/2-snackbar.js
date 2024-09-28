1170d336392d405a3194871e422c47bc44a8e1fa
svbiz4u <136245961+svbiz4u@users.noreply.github.com>
2024-09-28 17:54:49 +0200

    com-28092024-1754

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,3 +2,51 @@
 import iziToast from "izitoast";
 // Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
+
+
+const startButton = document.querySelector('button');
+const form = document.querySelector('form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const userTime = Number(form.delay.value);
+  const btnState = form.querySelector('input[name="state"]:checked');
+  const userBtnState = btnState.value;
+
+  const promise = new Promise((res, rej) => {
+    setTimeout(() => {
+      if (userBtnState === 'fulfilled') {
+        res(userTime);
+      } else {
+        rej(userTime);
+      }
+    }, userTime);
+  });
+
+  promise
+    .then(function (delay) {
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        fontSize: 'large',
+        close: true,
+        position: 'topRight',
+        messageColor: 'white',
+        timeout: 2000,
+        icon: false,
+      });
+    })
+    .catch(function (delay) {
+      iziToast.error({
+        message: `❌ Rejected promise in ${delay}ms`,
+        fontSize: 'large',
+        close: true,
+        position: 'topRight',
+        messageColor: 'white',
+        timeout: 2000,
+        icon: false,
+      });
+    });
+
+  form.reset();
+});
